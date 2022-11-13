2dce2b848313a30c1b16ae7decadcd2690d1f436
BrendaPSO <brenda_orlandi@hotmail.com>
2022-11-13 15:20:19 -0500

    first commit

---
+++ decrypt.py
@@ -0,0 +1,27 @@
+from cryptography.fernet import Fernet
+import os
+
+
+def retornarKey():
+    return open("key.key", "rb").read()
+
+
+def decrypt(items, key):
+    f = Fernet(key)
+    for item in items:
+        with open(item, 'rb') as file:
+            encrypted_data = file.read()
+        decrypted_data = f.decrypt(encrypted_data)
+        
+        with open (item, 'wb') as file:
+            file.write(decrypted_data)
+       
+if __name__=='__main__':
+    path_to_encrypt = '/home/kali/Documents/Projeto_SASI/Arquivos'
+    os.remove(path_to_encrypt+'/'+'readme.txt')
+    items = os.listdir(path_to_encrypt)
+    full_path = [path_to_encrypt+'/'+item for item in items]
+
+key = retornarKey()
+decrypt(full_path, key)
+
